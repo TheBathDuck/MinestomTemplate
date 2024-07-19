0935463449065151709bb73d1a4a2d49f558d8f6
OmeWillem <cryptoaafje@gmail.com>
2024-07-19 23:21:07 +0200

    Added wall signs

--- src/main/java/nl/thebathduck/minestom/blocks/placement/SignPlacement.java
+++ src/main/java/nl/thebathduck/minestom/blocks/placement/SignPlacement.java
@@ -1,12 +1,14 @@
 package nl.thebathduck.minestom.blocks.placement;
 
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.rule.BlockPlacementRule;
 import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class SignPlacement extends BlockPlacementRule {
@@ -30,6 +32,12 @@ public class SignPlacement extends BlockPlacementRule {
     public @Nullable Block blockPlace(@NotNull BlockPlacementRule.PlacementState placementState) {
         if (placementState.playerPosition() == null) return block;
         float yaw = placementState.playerPosition().yaw() + 180;
+
+        BlockFace face = placementState.blockFace();
+        if (face != BlockFace.TOP && face != BlockFace.BOTTOM) {
+            return replacements.get(block.namespace()).withProperty("facing", face.name().toLowerCase(Locale.ROOT));
+        }
+
         return block.withProperty("rotation", "" + ((int) Math.floor((yaw * 16.0F / 360.0F) + 0.5D) & 15));
     }
 
